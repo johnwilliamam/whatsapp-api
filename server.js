17f296b305b0b401d6de86de0c8dfb807f41ba85
John William de Araujo Mesquita <j.williamd.a.m@outlook.com>
2024-03-05 19:59:01 -0300

    whatsapp-api-free

---
+++ server.js
@@ -0,0 +1,17 @@
+const app = require('./src/app')
+const { baseWebhookURL } = require('./src/config')
+const cors = require('cors')
+require('dotenv').config()
+
+// Start the server
+const port = process.env.PORT || 3000
+
+// Check if BASE_WEBHOOK_URL environment variable is available
+if (!baseWebhookURL) {
+  console.error('BASE_WEBHOOK_URL environment variable is not available. Exiting...')
+  process.exit(1) // Terminate the application with an error code
+}
+app.use(cors())
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`)
+})
